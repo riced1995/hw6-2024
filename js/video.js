b304debeb950b2b2393593e71efa7f88fac64cc4
riced1995 <143537188+riced1995@users.noreply.github.com>
2024-04-11 23:07:07 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,12 +1,12 @@
 var video;
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
-	video = document.querySelector("#player1")
+	console.log("Good job opening the window");
+	video = document.querySelector("#player1");
 	video.autoplay = false;
 	video.loop = false;
-	console.log('auto play is set to'+ video.autoplay)
-	console.log('loop is set to'+ video.loop)
+	console.log('auto play is set to'+ video.autoplay);
+	console.log('loop is set to'+ video.loop);
 
 });
 
