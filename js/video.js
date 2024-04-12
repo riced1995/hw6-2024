6e1b5fe0a68b41cb106c07412886a8069f6e0605
riced1995 <143537188+riced1995@users.noreply.github.com>
2024-04-11 23:26:44 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -60,12 +60,11 @@ if (video) {
 });
 
 //Adjust Volume//
-document.querySelector("#slider").addEventListener("input", function () {
-	var volume = this.value / 100;
-	video.volume = volume;
-	var volumeSpan = document.getElementById("volume");
-	volumeSpan.textContent = this.value + "%";
-	console.log("Volume is " + volume);
+document.getElementById("slider").addEventListener("input", function () {
+	var volumeValue = this.value / 100;
+	document.getElementById("volume").textContent = volumeValue
+	video.volume = volumeValue;
+	console.log("Volume is set to", volumeValue);
 });
 
 //Old School//
